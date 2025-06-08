8384efc42ad28bb3dcca46ef9f3291fe66c616b6
Gavin <kakitgavinon99@gmail.com>
2025-06-09 02:19:43 +0800

    create attack timer for each entity, emit signal from entity and resolve damage calculation in game main script

--- scripts/entity.gd
+++ scripts/entity.gd
@@ -2,13 +2,18 @@ class_name Entity
 
 extends Node2D
 
+@onready var game: Node2D = $".."
+
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var health_bar: ProgressBar = $HealthBar
 @onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var attack_timer: Timer = $AttackTimer
 
 @export var size: float
 @export var max_hp: float
 @export var attackPower: float
+@export var attackSpeed: float
+@export var vfx: PackedScene
 
 var currentHealth
 
@@ -17,6 +22,11 @@ signal enemy_defeated
 func _ready() -> void:
 	sprite_2d.transform.x = Vector2(size, 0)
 	sprite_2d.transform.y = Vector2(0, size)
+	
+	if attack_timer:
+		attack_timer.start(1/attackSpeed)
+		print(name + ' attack timer started')
+		attack_timer.connect("timeout", game._on_entity_attack_timer_timeout.bind(name, attackPower))
 	
 	if health_bar:
 		currentHealth = max_hp
@@ -28,10 +38,9 @@ func _process(delta: float) -> void:
 	if currentHealth <= 0:
 		die()
 
-func attack(entity: Entity):
+func attack():
 	print(name + ' Attacked!')
 	animation_player.play('attack')
-	entity.currentHealth -= attackPower
 
 func die():
 	enemy_defeated.emit()
--- scripts/game.gd
+++ scripts/game.gd
@@ -1,13 +1,15 @@
 extends Node2D
 
-@onready var player_attack_timer: Timer = $PlayerAttackTimer
 @onready var player: Entity = $Player
 
 @onready var enemy_location: Node2D = $EnemyLocation
 
-@onready var enemy_attack_timer: Timer = $EnemyAttackTimer
+@onready var gold_label: Label = $Control/GoldLabel
+@onready var enemies_count_label: Label = $Control/EnemiesCountLabel
 
-@onready var gold_label: Label = $GoldLabel
+@onready var choose_upgrades: PanelContainer = $Control/ChooseUpgrades
+@onready var upgrade_attack: Button = $Control/ChooseUpgrades/VBoxContainer/UpgradeAttack
+@onready var next: Button = $Control/ChooseUpgrades/VBoxContainer/Next
 
 #implement dictionary to init preload texture in the future
 const orc = preload('res://scenes/entities/orc.tscn')
@@ -22,39 +24,62 @@ var enemy: Entity
 var entityArray: Array = [orc, skeletonSoldier, slime, golbin]
 
 var gold: int = 10
+var enemiesRemaining = 1
+var level = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	player_attack_timer.start(1)
-	enemy_attack_timer.start(5)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if !enemy:
+	if !enemy and enemiesRemaining != 0:
 		var i = randi_range(0, entityArray.size() - 1)
 		enemy = entityArray[i].instantiate()
 		enemy.enemy_defeated.connect(_on_enemy_defeated)
 		enemy.position = enemy_location.position
 		add_child(enemy)
+		#reset player attack timer after adding an enemy
+		player.attack_timer.start(1/player.attackSpeed)
 
 	gold_label.text = str(gold)
-
-
-func _on_player_attack_timer_timeout() -> void:
-	if enemy and player:
-		player.attack(enemy)
-		var vfx = hitEffect_1.instantiate()
-		vfx.position = enemy.position
-		add_child(vfx)
-
-
-func _on_enemy_attack_timer_timeout() -> void:
-	if enemy and player:
-		enemy.attack(player)
-		var vfx = darkBolt.instantiate()
-		vfx.position = player.position
-		add_child(vfx)
+	enemies_count_label.text = 'Enemies Remaining: ' + str(enemiesRemaining)
+	
+	if enemiesRemaining == 0:
+		choose_upgrades.visible = true
+	else:
+		choose_upgrades.visible = false
+		
+	if player.currentHealth <= 0:
+		get_tree().reload_current_scene()
 
 func _on_enemy_defeated():
 	gold += 1
+	enemiesRemaining -= 1
+
+
+func _on_upgrade_attack_pressed() -> void:
+	if gold >= 10:
+		player.attackPower += 1
+		gold -= 10
+	else:
+		print('Not enough gold')
+
+func _on_next_pressed() -> void:
+	enemiesRemaining = 2
+
+func _on_entity_attack_timer_timeout(entity, attackPower):
+	if player and enemy:
+		if entity == 'Player':
+			player.attack()
+			enemy.currentHealth -= attackPower
+			var vfx = hitEffect_1.instantiate()
+			vfx.position = enemy.position
+			add_child(vfx)
+		else:
+			enemy.attack()
+			player.currentHealth -= attackPower
+			var vfx = darkBolt.instantiate()
+			vfx.position = player.position
+			add_child(vfx)
